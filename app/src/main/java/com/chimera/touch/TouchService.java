575273a510867ed53a33c0c5b2d128b41ead8572
Traidepchetdoi <nguyenbicx2000@gmail.com>
2026-06-26 17:13:59 +0700

    Update TouchService.java
    Fix TouchService.java

--- app/src/main/java/com/chimera/touch/TouchService.java
+++ app/src/main/java/com/chimera/touch/TouchService.java
@@ -93,41 +93,4 @@ public class TouchService extends AccessibilityService {
         }
         @Override public void onStart() {}
     }
-}    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (wsServer != null) {
-            try { wsServer.stop(); } catch (Exception e) {}
-        }
-    }
-
-    private class WsServer extends WebSocketServer {
-        public WsServer(InetSocketAddress addr) { super(addr); }
-
-        @Override
-        public void onOpen(WebSocket conn, ClientHandshake h) {
-            Log.i(TAG, "Client connected");
-        }
-
-        @Override
-        public void onMessage(WebSocket conn, String msg) {
-            try {
-                // Format: "prevX,prevY,targetX,targetY"
-                String[] p = msg.split(",");
-                if (p.length == 4) {
-                    int tx = (int) Float.parseFloat(p[2]);
-                    int ty = (int) Float.parseFloat(p[3]);
-                    dispatchSwipe(tx, ty, 8);
-                }
-            } catch (Exception e) {
-                Log.e(TAG, "Parse error: " + e.getMessage());
-            }
-        }
-
-        @Override public void onClose(WebSocket conn, int c, String r, boolean b) {}
-        @Override public void onError(WebSocket conn, Exception e) {
-            Log.e(TAG, "WS error: " + e.getMessage());
-        }
-        @Override public void onStart() {}
-    }
 }
